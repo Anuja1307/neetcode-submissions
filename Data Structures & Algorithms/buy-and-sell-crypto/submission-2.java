dd3e6f048f83c8787acfa3f5d0882281e9da72c6
Anuja1307 <anujars541@gmail.com>
2026-05-02 08:31:06 +0530

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+
+        if(prices.length<2){
+            return 0;
+        }
+
+        int l=0;
+        int r=1;
+        int buy=prices[l];
+        int sell=prices[r];
+
+        int res=0;
+
+        while(l<prices.length && r<prices.length){
+
+              if(prices[l]>prices[r]){
+                prices[l]=prices[r];
+                l=r;
+                r++;
+              }
+              else{
+                res=Math.max(res,prices[r]-prices[l]);
+                r++;
+              }
+        }
+        return res;
+        
+    }
+}
