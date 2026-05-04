8e313a74508231a9595898dd0376306601790b76
Anuja1307 <anujars541@gmail.com>
2026-05-04 06:05:51 +0530

    Add: permutation-string - submission-3

---
+++ Data Structures & Algorithms/permutation-string/submission-3.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+
+        if(s1.length()>s2.length()){
+            return false;
+        }
+
+        int count1[]=new int[26];
+
+        for(int i=0;i<s1.length();i++){
+            count1[s1.charAt(i)-'a']++;
+        }
+
+        int count2[]=new int[26];
+
+        int w=s1.length();
+        int n=s2.length();
+
+        for(int i=0;i<w;i++){
+            count2[s2.charAt(i)-'a']++;
+        }
+
+        if(Arrays.equals(count1,count2)){
+            return true;
+        }
+        for(int i=w;i<n;i++){
+            count2[s2.charAt(i-w)-'a']--;
+            count2[s2.charAt(i)-'a']++;
+            if(Arrays.equals(count1,count2)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
