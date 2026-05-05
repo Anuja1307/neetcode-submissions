fcd2220e1f8b51d77eea48d5d9f640711a62a178
Anuja1307 <anujars541@gmail.com>
2026-05-05 07:28:38 +0530

    Add: validate-parentheses - submission-5

---
+++ Data Structures & Algorithms/validate-parentheses/submission-5.java
@@ -0,0 +1,48 @@
+class Solution {
+    public boolean isValid(String s) {
+
+        if(s.length()<2){
+            return false;
+        }
+
+        Stack<Character> stack=new Stack<>();
+
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='(' || s.charAt(i)=='{' || s.charAt(i) == '['){
+                stack.push(s.charAt(i));
+            }
+            else{
+                 if(!stack.isEmpty()){
+                         
+                
+
+                if(stack.peek()=='[' && s.charAt(i)!=']'){
+                    return false;
+                }
+                else if(stack.peek()=='{' && s.charAt(i)!='}'){
+                    return false;
+                }
+                else if (stack.peek()=='(' && s.charAt(i)!=')'){
+                    return false;
+                }
+                
+                stack.pop();
+                 }
+                else{
+                    return false;
+                }
+                
+            }
+        }
+
+        if(!stack.isEmpty()){
+            return false;
+        }
+       
+        return true;
+        
+
+
+        
+    }
+}
