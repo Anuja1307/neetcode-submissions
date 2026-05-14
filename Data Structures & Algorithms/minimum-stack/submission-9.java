1f90a27e5c6a6db571726dbc3cf079ffcacdb5cb
Anuja1307 <anujars541@gmail.com>
2026-05-15 05:18:19 +0530

    Add: minimum-stack - submission-9

---
+++ Data Structures & Algorithms/minimum-stack/submission-9.java
@@ -0,0 +1,37 @@
+class MinStack {
+
+Stack<Integer> stack ;
+Stack<Integer> minstack;
+    
+
+    public MinStack() {
+        stack=new Stack<>();
+        minstack=new Stack<>();
+    }
+    
+    public void push(int val) {
+        stack.push(val);
+        if(minstack.isEmpty() || minstack.peek()>=val){
+            minstack.push(val);
+        }
+        
+    }
+    
+    public void pop() {
+        int n=stack.pop();
+        if(minstack.peek()==n){
+            minstack.pop();
+        }
+        
+    }
+    
+    public int top() {
+        return stack.peek();
+        
+    }
+    
+    public int getMin() {
+        return minstack.peek();
+        
+    }
+}
