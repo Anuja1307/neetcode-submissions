e9cf674ab41e6e0ec92a29d07027fcdd4f884398
Anuja RS <anujars541@gmail.com>
2026-06-15 13:51:12 +0530

    Add: evaluate-reverse-polish-notation - submission-16

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-16.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int evalRPN(String[] tokens) {
+        List<String> arr = new ArrayList<>(Arrays.asList(tokens));
+        int i = 0;
+        while (arr.size() > 1) {
+
+            int j = i;
+
+            while (! "+-*/".contains(arr.get(j))) {
+                j++;
+            }
+
+            int result;
+
+            if (arr.get(j).equals("+")) {
+                result = Integer.parseInt(arr.get(j - 2))
+                        + Integer.parseInt(arr.get(j - 1));
+            }
+
+            else if (arr.get(j).equals("-")) {
+                result = Integer.parseInt(arr.get(j - 2))
+                        - Integer.parseInt(arr.get(j - 1));
+            }
+
+            else if (arr.get(j).equals("*")) {
+                result = Integer.parseInt(arr.get(j - 2))
+                        * Integer.parseInt(arr.get(j - 1));
+            }
+
+            else {
+                result = Integer.parseInt(arr.get(j - 2))
+                        / Integer.parseInt(arr.get(j - 1));
+            }
+            arr.remove(j - 1);
+            arr.remove(j - 2);
+             arr.set(j - 2, String.valueOf(result));
+            i = 0;
+        }
+
+        return Integer.parseInt(arr.get(0));
+    }
+}
