87a6ebd493e6ae685babbac8a3ca0d82f72bf522
Anuja RS <anujars541@gmail.com>
2026-07-27 05:55:16 +0530

    Add: buy-and-sell-crypto - submission-14

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-14.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+
+        if(prices.length<2){
+            return 0;
+        }
+
+        int l=0;
+        int r=1;
+        int res=0;
+
+        while(l<prices.length && r<prices.length){
+
+              if(prices[l]>prices[r]){
+                
+                l=r;
+                r++;
+              }
+              else{
+                res=Math.max(res,prices[r]-prices[l]);
+                r++;
+              }
+        }
+        return res;
+        
+    }
+}
+
